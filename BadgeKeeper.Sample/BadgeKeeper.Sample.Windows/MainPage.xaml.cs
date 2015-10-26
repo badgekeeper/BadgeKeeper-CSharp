7f5d3926c735c3bb116a3b102a4263f59958c65d
Alexander Pukhov <services@badgekeeper.net>
2015-10-26 13:16:00 +0300

    Sample application for Windows & Windows Phone

---
+++ BadgeKeeper.Sample/BadgeKeeper.Sample.Windows/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BadgeKeeper.Sample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void GetProjectAchievementsClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Windows.UI.Popups.MessageDialog("project achievements");
+            await dialog.ShowAsync();
+        }
+
+        private void GetUserAchievementsClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PostUserValuesClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void IncrementUserValuesClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
