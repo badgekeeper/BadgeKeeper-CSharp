e9a12dd7723cddee9ca3b59ab50124e3c9d87602
Alexander Pukhov <services@badgekeeper.net>
2015-11-06 01:32:48 +0300

    Ready for internal tests

--- Sources/BadgeKeeper/Network/BadgeKeeperApiService.cs
+++ Sources/BadgeKeeper/Network/BadgeKeeperApiService.cs
@@ -13,12 +13,19 @@
 // limitations under the License.
 
 using System;
-using System.Net;
 using System.Text;
 using System.Collections.Generic;
 using System.Runtime.Serialization.Json;
 using BadgeKeeper.Objects;
 using BadgeKeeper.Objects.Models;
+
+#if !(PORTABLE40 || PORTABLE)
+using System.Net;
+#else
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+#endif
 
 namespace BadgeKeeper.Network
 {
@@ -67,27 +74,18 @@ namespace BadgeKeeper.Network
                 // handler
                 client.DownloadStringCompleted += (s, e) =>
                 {
-                    try
-                    {
-                        BadgeKeeperResponse<ResponseType> result = FromJson<BadgeKeeperResponse<ResponseType>>(e.Result);
-                        if (result.Error != null)
-                        {
-                            onError(result.Error);
-                        }
-                        else
-                        {
-                            onSuccess(result.Result);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        onError(new BadgeKeeperResponseError(-1, ex.Message));
-                    }
+                    ProcessNonPortableResponse(e.Result, onSuccess, onError);
                 };
                 // make request
                 client.DownloadStringAsync(new Uri(url));
             }
 #else
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.GetAsync(url).ContinueWith(task => ProcessPortableResponse(task, onSuccess, onError));
+            }
 #endif
         }
 
@@ -102,30 +100,79 @@ namespace BadgeKeeper.Network
                 // handler
                 client.DownloadStringCompleted += (s, e) =>
                 {
-                    try
-                    {
-                        BadgeKeeperResponse<ResponseType> result = FromJson<BadgeKeeperResponse<ResponseType>>(e.Result);
-                        if (result.Error != null)
-                        {
-                            onError(result.Error);
-                        }
-                        else
-                        {
-                            onSuccess(result.Result);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        onError(new BadgeKeeperResponseError(-1, ex.Message));
-                    }
+                    ProcessNonPortableResponse(e.Result, onSuccess, onError);
                 };
                 // make request
                 client.UploadStringAsync(new Uri(url), "POST", body);
             }
 #else
-
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.PostAsync(url, new StringContent(body)).ContinueWith(task => ProcessPortableResponse(task, onSuccess, onError));
+            }
 #endif
         }
+
+#if (PORTABLE40 || PORTABLE)
+        /// <summary>
+        /// Process response message for portable library
+        /// </summary>
+        /// <typeparam name="ResponseType"></typeparam>
+        /// <param name="task">Response task result</param>
+        /// <param name="onSuccess">Success callback</param>
+        /// <param name="onError">Error callback</param>
+        private void ProcessPortableResponse<ResponseType>(Task<HttpResponseMessage> task, Action<ResponseType> onSuccess, Action<BadgeKeeperResponseError> onError)
+        {
+            if (task.Result.IsSuccessStatusCode)
+            {
+                task.Result.Content.ReadAsStringAsync().ContinueWith(content =>
+                {
+                    BadgeKeeperResponse<ResponseType> response = FromJson<BadgeKeeperResponse<ResponseType>>(content.Result);
+                    if (response.Error != null)
+                    {
+                        onError(response.Error);
+                    }
+                    else
+                    {
+                        onSuccess(response.Result);
+                    }
+                });
+            }
+            else
+            {
+                onError(new BadgeKeeperResponseError(-1, "Internal server error."));
+            }
+        }
+#else
+        /// <summary>
+        /// Process response message for non portable library
+        /// </summary>
+        /// <typeparam name="ResponseType"></typeparam>
+        /// <param name="response">Response string for request</param>
+        /// <param name="onSuccess">Success callback</param>
+        /// <param name="onError">Error callback</param>
+        private void ProcessNonPortableResponse<ResponseType>(string response, Action<ResponseType> onSuccess, Action<BadgeKeeperResponseError> onError)
+        {
+            try
+            {
+                BadgeKeeperResponse<ResponseType> result = FromJson<BadgeKeeperResponse<ResponseType>>(response);
+                if (result.Error != null)
+                {
+                    onError(result.Error);
+                }
+                else
+                {
+                    onSuccess(result.Result);
+                }
+            }
+            catch (Exception ex)
+            {
+                onError(new BadgeKeeperResponseError(-1, ex.Message));
+            }
+        }
+#endif
 
         private static string ShouldLoadIcons(bool shouldLoadIcons)
         {
